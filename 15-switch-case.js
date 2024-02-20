fbe8d5287907cefc97fba9ce80ec2adc0b4a2476
pradeepsutharr <psuthar318@gmail.com>
2024-02-20 10:02:15 +0530

    iife

---
+++ 15-switch-case.js
@@ -0,0 +1,35 @@
+let inp = "3";
+
+switch (inp) {
+  case "1":
+    // console.log("himu");
+    msg = "himu";
+    break;
+  // break will break the line
+
+  case "2":
+    // console.log("aksh");
+    msg = "aksh";
+    break;
+
+  case "3":
+    // console.log("bou");
+    msg = "bou";
+    break;
+
+  case "4":
+    // console.log("04_error");
+    msg = "04error";
+    break;
+
+  case "5":
+    // console.log("parth");
+    msg = "parth";
+    break;
+
+  default:
+    // console.log("voyo ja");
+    msg = "Malik Khami Jao, 5 sudhi j che";
+    break;
+}
+console.log(msg);
